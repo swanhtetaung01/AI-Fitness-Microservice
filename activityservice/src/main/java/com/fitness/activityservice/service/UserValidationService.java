71922d1ce3ee1fe9239b0128e19368e305203c3d
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-11-25 23:21:20 +0700

    Add logs in userservice/.../UserService.java and activityservice/.../UserValidationService.java

--- activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -1,6 +1,7 @@
 package com.fitness.activityservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,10 +9,12 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserValidationService {
     private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId) {
+        log.info("Calling user validation API for userId: {}", userId);
         try{
             return userServiceWebClient.get()
                     .uri("api/users/{userId}/validate", userId)
